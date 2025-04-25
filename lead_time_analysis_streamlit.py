429a13d3ca71b884f516bf93b3b1fee9564e7516
PARK EUI CHEOL <108806935+parkeuicheol@users.noreply.github.com>
2025-04-25 10:37:44 +0900

    Update lead_time_analysis_streamlit.py

--- lead_time_analysis_streamlit.py
+++ lead_time_analysis_streamlit.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = 'Malgun Gothic'
 import matplotlib as mpl
 mpl.rcParams['axes.unicode_minus'] = False
-import io, base64, os
+import io, base64
 
 # ------------------------------------------------------------------
 # 엑셀 생성: 이미지 포함해서 Bytes 반환
